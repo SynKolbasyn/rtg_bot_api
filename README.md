cc23e9e9adc30a557f536acf08bf5c4fcaab1618
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2024-09-02 16:27:32 +0300

    feat: Api generation program added

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Rust telegram bot api (rtg_bot_api)
 The library provides asynchronous access to the telegram bot api
+
+## Generate api
+To generate the latest version of the api, use the following command
+```Bash
+cargo run --bin parse_tg_bot_api
+```
---
+++ src/bin/parse_tg_bot_api/main.rs
@@ -0,0 +1,84 @@
+//!   Rust telegram bot api. The library provides asynchronous access to the telegram bot api.
+//!   Copyright (C) 2024  Andrew Kozmin
+//!
+//!   This program is free software: you can redistribute it and/or modify
+//!   it under the terms of the GNU Affero General Public License as published by
+//!   the Free Software Foundation, either version 3 of the License, or
+//!   (at your option) any later version.
+//!
+//!   This program is distributed in the hope that it will be useful,
+//!   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//!   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//!   GNU Affero General Public License for more details.
+//!
+//!   You should have received a copy of the GNU Affero General Public License
+//!   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+mod tg_api;
+
+
+use std::collections::HashSet;
+
+use anyhow::{Result, bail, Context};
+use reqwest::Response;
+use select::{
+  document::Document,
+  node::Node,
+  predicate::Attr,
+};
+
+// use crate::tg_api::{Type, Method};
+
+
+#[tokio::main]
+async fn main() {
+  match main_wraper().await {
+    Ok(_) => println!("PARSE SUNCCESS!"),
+    Err(e) => eprintln!("{e}"),
+  }
+}
+
+
+async fn main_wraper() -> Result<()> {
+  let html: String = get_html().await?;
+  let elements: Vec<Node> = get_list_of_main_tags(&Document::from(html.as_str()))?;
+  // let (types, methods): (HashSet<Type>, HashSet<Method>);
+
+  Ok(())
+}
+
+
+async fn get_html() -> Result<String> {
+  let url: String = String::from("https://core.telegram.org/bots/api");
+  let response: Response = reqwest::get(&url).await?;
+  
+  if !response.status().is_success() {
+    bail!("ERROR: Request to {} failed with {}", url, response.status());
+  }
+
+  let html: String = response.text().await?;
+  Ok(html)
+}
+
+
+fn get_list_of_main_tags(document: &Document) -> Result<Vec<Node>> {
+  let mut result: Vec<Node> = Vec::new();
+  let necessary_tags: HashSet<&str> = HashSet::from(["h4", "p", "table"]);
+  let document: Node = document.find(Attr("id", "dev_page_content")).next().context("ERROR: Couldn't find the start tag of the data")?;
+
+  for tag in document.children() {
+    let tag_name: &str = match tag.name() {
+      Some(name) => name,
+      None => continue,
+    };
+
+    if !necessary_tags.contains(tag_name) {
+      continue;
+    }
+
+    result.push(tag);
+  }
+
+  Ok(result)
+}
